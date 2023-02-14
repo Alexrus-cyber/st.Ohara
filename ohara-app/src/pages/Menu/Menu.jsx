1aec42a196f9ac35324e3bedea263b12cb5bd115
Vladlislav <vladlen4ig@gmail.com>
2023-02-14 18:37:25 +0300

    подготовка redux

--- ohara-app/src/pages/Menu/Menu.jsx
+++ ohara-app/src/pages/Menu/Menu.jsx
@@ -1,16 +1,23 @@
-import React, {useState, useCallback} from 'react';
+import React, {useState, useCallback, useEffect} from 'react';
 import ImageViewer from 'react-simple-image-viewer';
 import styles from './Menu.module.css'
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {clearData, getMenuData, listImagesSelector} from "../../slices/menu";
 
 export const Menu = () => {
     const [currentImage, setCurrentImage] = useState(0);
     const [isViewerOpen, setIsViewerOpen] = useState(false);
-    const {images} = useSelector(state => state.menuReducer)
 
+    const dispatch = useDispatch();
+
+    //делаем запрос на получение файлов в нашем случае картинки из моков вытаскиваем
+    useEffect(() => {
+        dispatch(getMenuData());
+    }, [dispatch])
+
+    const images = useSelector(listImagesSelector);
 
     const openImageViewer = useCallback((index) => {
-        console.log(index)
         setCurrentImage(index);
         setIsViewerOpen(true);
     }, []);
@@ -18,8 +25,12 @@ export const Menu = () => {
     const closeImageViewer = () => {
         setCurrentImage(0);
         setIsViewerOpen(false);
-        console.log(currentImage)
     };
+
+    //пример как юзать экшены
+    const handleClickClearImages = () => {
+        dispatch(clearData())
+    }
 
     return (
         <section className={styles.menu}>
@@ -47,6 +58,7 @@ export const Menu = () => {
                         />
                     )}
                 </div>
+                {images.length ? <button onClick={handleClickClearImages}>Удалить меню</button> : null}
             </div>
         </section>
 
--- ohara-app/src/redux/store.js
+++ ohara-app/src/redux/store.js
@@ -1,14 +1,30 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import menuReducer from '../slices/menu'
+import {combineReducers, configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
+import menuReducer  from '../slices/menu'
 import galleryReducer from '../slices/gallery'
 import newsReducer from '../slices/news'
+import storage from 'redux-persist/lib/storage';
+import {persistReducer} from "redux-persist";
 
 const rootReducer = combineReducers({
-    menuReducer,
+    menu: menuReducer,
     galleryReducer,
     newsReducer
 })
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: [ //почитай что такое whiteList тут уже в лс все сохраняется)
+        'menu'
+    ],
+};
+
+export const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware({
+        serializableCheck: false,
+    }),
 })
