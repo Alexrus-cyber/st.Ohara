a953465daa4ba5a9381fef96306ed93468df84c3
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2023-03-29 22:07:23 +0300

    NavBar test

--- ohara-app/src/admin/pages/components/Navbar/Navbar.jsx
+++ ohara-app/src/admin/pages/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import styles from './Navbar.module.css';
 import admin from '../../../../assets/admin.png'
 import {NavLink} from "react-router-dom";
+import {AppBar, Drawer, IconButton, Toolbar, Typography, useTheme} from "@mui/material";
+import {useState} from "react";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
+const drawerWidth = 240;
 export const Navbar = () => {
     const arr = [
         {id: 5, src: "/landingAdmin", text: "Главная"},
@@ -10,15 +16,68 @@ export const Navbar = () => {
         {id: 3, src: "/galleryAdmin", text: "Галерея"},
         {id: 4, src: "/reservationAdmin", text: "Бронирование"},
     ]
+    const theme = useTheme();
+    const [open, setOpen] = useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
     return (
-        <div className={styles.navbar}>
-            <h1 className={styles.title}>St.O'hara</h1>
-            <img className={styles.img} src={admin} alt={"admin"}/>
-            <p className={styles.name}>Анастасия Михайлова</p>
-            <p className={styles.position}>Официант</p>
-            <div className={styles.links}>
-                {arr.map(e =>  <NavLink key={e.id} to={e.src} className={({isActive}) => (isActive ? styles.active : styles.link)}>{e.text}</NavLink>)}
+        <>
+            <AppBar style={{backgroundColor: "transparent", boxShadow: "unset"}} className={styles.secret} position="fixed" open={open}>
+                <Toolbar>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        edge="start"
+                        sx={{ mr: 2, ...(open && { display: "none" }) }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer className={styles.secret}
+                sx={{
+                    flexShrink: 0,
+                    "& .MuiDrawer-paper": {
+                        width: drawerWidth,
+                        boxSizing: "border-box"
+                    }
+                }}
+                variant="persistent"
+                anchor="left"
+                open={open}
+            >
+                <IconButton onClick={handleDrawerClose}>
+                    {theme.direction === "ltr" ? (
+                        <ChevronLeftIcon />
+                    ) : (
+                        <ChevronRightIcon />
+                    )}
+                </IconButton>
+                <div className={styles.navbarSecret}>
+                    <h1 className={styles.title}>St.O'hara</h1>
+                    <img className={styles.img} src={admin} alt={"admin"}/>
+                    <p className={styles.name}>Анастасия Михайлова</p>
+                    <div className={styles.links}>
+                        {arr.map(e =>  <NavLink key={e.id} to={e.src} className={({isActive}) => (isActive ? styles.active : styles.link)}>{e.text}</NavLink>)}
+                    </div>
+                </div>
+            </Drawer>
+            <div className={styles.navbar}>
+                <h1 className={styles.title}>St.O'hara</h1>
+                <img className={styles.img} src={admin} alt={"admin"}/>
+                <p className={styles.name}>Анастасия Михайлова</p>
+                <div className={styles.links}>
+                    {arr.map(e =>  <NavLink key={e.id} to={e.src} className={({isActive}) => (isActive ? styles.active : styles.link)}>{e.text}</NavLink>)}
+                </div>
             </div>
-        </div>
+        </>
+
     )
 }
