e1595a2e0845dd8a0b62fdfec428afb611316d39
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2023-05-07 23:23:53 +0300

    Fixes

--- ohara-app/src/admin/pages/Reservation/ModalReservation/Inputs/index.js
+++ ohara-app/src/admin/pages/Reservation/ModalReservation/Inputs/index.js
@@ -1,11 +1,12 @@
 import {
   maxLength,
+  minLength,
   Required,
 } from "../../../Auth/components/Validators/Validators";
 import { InputUI } from "../../../Auth/components/Form/FormCreators";
 
 export const maxLength100 = maxLength(100);
-export const maxLength12 = maxLength(12);
+export const maxLength12 = maxLength(11);
 
 export const reservationInputs = [
   {
@@ -18,24 +19,32 @@ export const reservationInputs = [
   },
   {
     id: 2,
-    name: "fio",
-    placeholder: "ФИО",
+    name: "name",
+    placeholder: "Имя",
     validators: [Required, maxLength100],
     typeButton: InputUI,
     typeInput: "material",
   },
   {
     id: 3,
-    name: "phoneNumber",
-    placeholder: "Номер телефона",
-    validators: [Required, maxLength12],
+    name: "surname",
+    placeholder: "Фамилия",
+    validators: [Required, maxLength100],
+    typeButton: InputUI,
+    typeInput: "material",
+  },
+  {
+    id: 4,
+    name: "patronymic",
+    placeholder: "Отчество",
+    validators: [Required, maxLength100],
     typeButton: InputUI,
     typeInput: "material",
   },
 ];
 export const reservationInputsRight = [
   {
-    id: 4,
+    id: 5,
     name: "date",
     placeholder: "Дата",
     validators: [Required, maxLength100],
@@ -43,7 +52,7 @@ export const reservationInputsRight = [
     typeInput: "materialDate",
   },
   {
-    id: 5,
+    id: 6,
     name: "time",
     placeholder: "Время",
     validators: [Required, maxLength100],
@@ -51,11 +60,19 @@ export const reservationInputsRight = [
     typeInput: "materialTime",
   },
   {
-    id: 6,
+    id: 7,
     name: "amount",
     placeholder: "Количество людей",
     validators: [Required, maxLength100],
     typeButton: InputUI,
     typeInput: "materialPicker",
+  },
+  {
+    id: 8,
+    name: "phoneNumber",
+    placeholder: "Номер телефона",
+    validators: [Required, maxLength12, minLength(11)],
+    typeButton: InputUI,
+    typeInput: "material",
   },
 ];
